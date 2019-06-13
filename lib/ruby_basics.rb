ee54c035534f1bbea34e894a510c93288a97c049
Amin Amos <aminamosahs@gmail.com>
2019-06-13 02:19:23 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+ num1 / num2
 end
 
 def assign_variable(value)
-  
+  stuff = "#{value}"
 end
 
-def argue
-  
+def argue(is)
+  is
 end
 
-def greeting
-
+def greeting(gtg, name)
+  puts "${gtg}, ${name}"
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(defaultvalue = "cheese")
+  defaultvalue
 end
